4ee77487255814fd86cfb1e1f1705caab94e7184
rzaffari <r.z.irigaray@gmail.com>
2014-04-29 12:15:48 -0300

    init commit

---
+++ js/grid.js
@@ -0,0 +1,92 @@
+function Grid(size) {
+  this.size = size;
+  this.cells = this.clearGrid();
+}
+
+Grid.prototype = {
+
+	clearGrid: function () {
+		var cells = [];
+
+		for (var x = 0; x < this.size; x++) {
+			var row = cells[x] = [];
+
+			for (var y = 0; y < this.size; y++) {
+				row.push(new Tile({ x:x, y:y }));
+			};
+		};
+
+		return cells;
+	},
+
+	forEachCell: function(callback) {
+		for (var x = 0; x < this.size; x++) {
+			for (var y = 0; y < this.size; y++) {
+				callback(this.cells[x][y]);
+			}
+		}
+	},
+
+	tileAt: function(cell) {
+		return this.cells[cell.x][cell.y];
+	},
+
+
+	neighborsOf: function(cell) {
+		var cells = []
+			, maxX = cell.x + 2
+			, maxY = cell.y + 2;
+
+		for (var row = cell.x-1; row < maxX; row++) {
+			for (var col = cell.y-1; col < maxY; col++) {
+				var candidate = { x: row, y: col };
+				
+				if(this.withinBounds(candidate)) {
+					cells.push(candidate);
+				}
+			};
+		};
+
+		return cells;
+	},
+
+	mineCells: function() {
+		var mines = [];
+
+		this.forEachCell(function (tile) {
+			if (tile.isMine()) {
+				mines.push({ x: tile.x, y: tile.y });
+			}
+		});
+
+		return mines;
+	},
+
+	randomAvailableCell: function () {
+		var cells = this.availableCells();
+
+		if (cells.length) {
+			return cells[Math.floor(Math.random() * cells.length)];
+		}
+	},
+
+	availableCells: function () {
+		var cells = [];
+
+		this.forEachCell(function (tile) {
+			if (!tile.isMine()) {
+				cells.push({ x: tile.x, y: tile.y });
+			}
+		});
+
+		return cells;
+	},
+
+	withinBounds: function (position) {
+	  return position.x >= 0 && position.x < this.size &&
+	         position.y >= 0 && position.y < this.size;
+	}
+
+};
+
+ 
